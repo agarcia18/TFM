334fda3aff04e69e6c97cad2555dc5b9babbf906
agarcia18 <ainhoa.garcia18@gmail.com>
2021-04-16 19:50:27 +0200

    Upload BED files, total output

--- Visual EccDNA/server.R
+++ Visual EccDNA/server.R
@@ -1,19 +1,61 @@
 server <- function(input, output){
   
-  total_circles<-circ %>% count()
-  p_bad <-((circ %>% filter (quality=="Bad") %>% count())/total_circles)*100
-  p_low <- ((circ %>% filter(quality=="Low") %>% count())/total_circles)*100
-  p_medium <- ((circ %>% filter(quality=="Medium") %>% count())/total_circles)*100
-  p_good <- ((circ %>% filter(quality=="Good") %>% count())/total_circles)*100
-  
-  filter_circ <- reactive(circ %>% filter(quality %in% input$quality))
-  filter_circ <- reactive(circ[circ$size_bp>=input$size[1]&circ$size_bp<=input$size[2],])
-  
+    dataframe<-reactive({
+      if (is.null(input$bedfile))
+        return(NULL)                
+     circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
+     names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
+     
+     circ
+     
+    })
+    
+    output$ui_total <- renderUI({
+      req(input$bedfile)
+      circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
+      
+      valueBox(value=circ %>% count(),tags$b("TOTAL DNA CIRCLES"), color="maroon",
+                   icon = tags$i(class = "fas fa-dna", style="font-size: 50px")
+          )
+      })
+    
+    output$table <- renderUI({
+      req(input$bedfile)
+      circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
+      
+      box(title = "Summary", width = NULL, solidHeader = TRUE,
+          tableOutput("head"))
+    })
+    
+    
   output$results <- renderPlot({
-    ggplot(filter_circ(), aes( x = size_bp, y = coverage_cont))+
-      geom_point(colour="#E84E84")+
-      geom_point(colour="white",size=0.6)+
-      theme_bw() + theme(legend.position =c(0.15,0.85),legend.title = element_blank(),panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
-  })
+    if (is.null(input$bedfile))
+      return(NULL)                
+    circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
+    names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
+    
+    # Set chromosome as factor and fix random chr
+    circ$chr <- substr(circ$chr, start = 1, stop = 5)
+    circ$chr<-str_remove(circ$chr,"_")
+    circ$chr<-str_remove(circ$chr,"chr")
+    circ$chr <- factor(circ$chr, levels = c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","Un","M","X","Y"))
+    
+    
+    # Add quality levels (score < 10 = Bad, score < 50 = Low, score < 200 = Medium, score > 200 = Good)
+    circ$quality <- cut(circ$score,breaks=c(-Inf,10,50,200,Inf),labels= c("Bad","Low", "Medium", "Good"),right = FALSE)
+    
+    # Add size of circle (number of bp)
+    circ$size_bp <- circ$end - circ$start
+    
+    circ
+    
+      ggplot(circ, aes(x = chr, y = size_bp))+
+        geom_point (aes(color=quality))+
+        theme_minimal()
+    })
+    
   
+  output$head <- renderTable({
+    head(dataframe())
+  })
 }
--- Visual EccDNA/ui.R
+++ Visual EccDNA/ui.R
@@ -3,45 +3,9 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(ggplot2)
+library(ggiraph)
 library(plotly)
-library(randDNA)
 
-circ <- as.data.frame(read.table("C:/Users/ainho/eccDNA/BED/SRR6315412_unknown_circle.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE))
-
-# Change names
-names(circ)[1] <- "chr"
-names(circ)[2] <- "start"
-names(circ)[3] <- "end"
-names(circ)[4] <- "discordant_reads"
-names(circ)[5] <- "split_reads"
-names(circ)[6] <- "score"
-names(circ)[7] <- "coverage_mean"
-names(circ)[8] <- "coverage_sd"
-names(circ)[9] <- "coverage_start"
-names(circ)[10] <- "coverage_end"
-names(circ)[11] <- "coverage_cont"
-
-# Set chromosome as factor
-circ$chr <- substr(circ$chr, start = 1, stop = 5)
-circ$chr<-str_remove(circ$chr,"_")
-circ$chr<-str_remove(circ$chr,"chr")
-circ$chr <- factor(circ$chr, levels = c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","Un","M","X","Y"))
-
-# Add quality levels (score < 10 = Bad, score < 50 = Low, score < 200 = Medium, score > 200 = Good)
-circ$quality <- cut(circ$score,breaks=c(-Inf,10,50,200,Inf),labels= c("Bad","Low", "Medium", "Good"),right = FALSE)
-
-# Add size of circle (number of bp)
-size_bp <- circ$end - circ$start
-circ<- add_column(circ,size_bp)
-head(circ)
-
-
-
-# Get Chromosome Input choices
-all <- "All"
-chr_choices <- append(all,levels(circ$chr))
-
-# Random sequence
 
 # User interface
 ui <- dashboardPage(
@@ -49,8 +13,8 @@ ui <- dashboardPage(
     
     dashboardSidebar(
         sidebarMenu(
-            menuItem("About",tabName="about",icon=icon("tasks")),
-            menuItem("Results", tabName ="results", icon=icon("bar-chart")),
+            menuItem("Get started",tabName="about",icon=icon("tasks")),
+            menuItem("View results", tabName ="results", icon=icon("bar-chart")),
             menuItem("Circle Info",tabName="circle",icon=icon("circle-o"))
         )
     ),
@@ -59,49 +23,27 @@ ui <- dashboardPage(
         tabItems(
             tabItem(
                 tabName = "about",
+                    fluidRow(h2("Wellcome!")),
                     fluidRow(
-                        h2("Wellcome!")),
-                    fluidRow(
-                        box(title= "Upload a Circle BED file with output:",fileInput("bedfile","Choose file:"))
-                    ),
+                        box(title= "Upload a Circle BED file with output:",fileInput("bedfile","Choose file:"),status="primary"),
+                        uiOutput("ui_total")
+            ),
+                    fluidRow(uiOutput("table"))
             ),
             tabItem(
                 tabName = "results",
                     fluidRow(
-                        valueBox(total_circles,tags$b("TOTAL DNA CIRCLES"), color="maroon",
-                                 icon = tags$i(class = "fas fa-dna", style="font-size: 50px")
-                                 ),
-                        box(width = 3,background = "olive",tags$h4("GOOD:",paste0(round(p_good,2),"%"))),
-                        box(width = 3, background = "yellow",tags$h4("LOW:", paste0(round(p_low,2),"%"))),
-                        box(width = 3, background = "light-blue",tags$h4("MEDIUM:",paste0(round(p_medium,2),"%"))),
-                        box(width = 3, background = "red",tags$h4("BAD:", paste0(round(p_bad,2),"%"))),
-                        ),
-                    fluidRow(
                         box(plotOutput("results"),width=7,
                             status = "primary",
                             ),
-                        box(title= span(icon("filter"), "Filters"), status = "warning", width = 5,
-                            checkboxGroupInput(inputId = "quality",
-                                               "Quality of your circles:",
-                                               choices = c("Bad","Low","Medium","Good"),
-                                               selected= c("Medium","Good")
-                            ),
-                            sliderInput(inputId = "size",
-                                        "By size (number of base pairs):",
-                                        min = min(circ$size_bp),
-                                        max= max(circ$size_bp),
-                                        value = c(min(circ$size_bp),max(circ$size_bp))),
-                            selectInput(inputId = "chr",
-                                        "By source chromosome:",
-                                        choices = chr_choices),
-                           
-    )
-                ),
+                    ),
             ),
             tabItem(
                 tabName="circle",
                         h2("Here goes the info of each circle")
-                    )
+                )
         )
     )
 )
+
+
