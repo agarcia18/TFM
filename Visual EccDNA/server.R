6a45e2fcfd0de2923aaee45ae8eeefccf7e567c0
agarcia18 <ainhoa.garcia18@gmail.com>
2021-04-19 13:09:10 +0200

    PEC2

--- Visual EccDNA/server.R
+++ Visual EccDNA/server.R
@@ -4,36 +4,49 @@ server <- function(input, output,session){
   
 ##### "Get started" tab server functions ####
     
-  # Total number of circles ValueBox
+  # TOTAL CIRCLES VALUEBOX
     output$total <- renderUI({
+      
+      # Read data and count()
       req(input$bedfile)
       circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
       
       valueBox(value=circ %>% count(),tags$b("TOTAL DNA CIRCLES"), color="navy",width=6,
                icon = tags$i(class = "fas fa-dna", style="color:white"))
       })
-  # Get started button
+    
+  # RESULTS BUTTON
     output$start<-renderUI({
       req(input$bedfile)
-      box(status="primary", background = "light-blue",height=100,width=4,
-          "Click this button to go to View Results:",br(),
-          actionButton("click","Get Started!")
+      box(background = "light-blue",height=120,width=4,
+          "Click this button to see results:",br(),br(),
+          actionButton("click","Go to View Results")
           )
       })
-  # Quality plot
+    
+    # Enable going to View Results tab after click
+    observeEvent(input$click, {
+      newtab <- switch(input$tabs,
+                       "about" = "results")
+      
+      updateTabItems(session, "tabs", newtab)
+    })
+    
+  # QUALITY PLOT
     output$quality_gg<-renderUI({
+      
+      # Read data and add labels
       req(input$bedfile)
       if (is.null(input$bedfile))
         return(NULL)                
       circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
-      
-      # Add column names
       names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
       
       # Add quality levels (score < 10 = Bad, score < 50 = Low, score < 200 = Medium, score > 200 = Good)
       circ$quality <- cut(circ$score,breaks=c(-Inf,10,50,200,Inf),labels= c("Bad","Low", "Medium", "Good"),right = FALSE)
       
-      box(collapsible=TRUE,title="Quality in DNA circles",height=250,renderPlot(
+      # Make the box and the plot 
+      box(title="Quality in DNA circles",height=250,renderPlot(
         ggplot(circ, aes(x = quality)) +  
           geom_bar(aes(y = (..count..)/sum(..count..),fill=quality),width=0.4,alpha=0.8,position = position_stack(reverse = TRUE))+
           scale_fill_manual(values=c("#FF5733","#FFC300","#DAF7A6","#6EBC63"))+
@@ -44,15 +57,13 @@ server <- function(input, output,session){
         ,height=200))
     })
   
-  # Data table
+  # DATA TABLE
     
     # Import data from file and add labels
     dataframe<-reactive({
       if (is.null(input$bedfile))
         return(NULL)                
       circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
-      
-      # Add column names
       names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
       
       # Add circle id
@@ -63,8 +74,8 @@ server <- function(input, output,session){
       
       # Add size of circle (number of bp)
       circ$size_bp <- circ$end - circ$start
-      circ
       
+      circ
     })
     
     # Make the data table
@@ -84,33 +95,113 @@ server <- function(input, output,session){
           dataTableOutput("data"))
     })
     
+
     
-##### "Results" tab server functions ####  
+##### "View Results" tab server functions ####  
+  
+  # PLOT OUTPUT 
     
-  output$results <- renderPlot({
-   
-    if (is.null(input$bedfile))
-      return(NULL)                
-    circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
-    names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
-    
-    # Set chromosome as factor and fix random chr
-    circ$chr <- substr(circ$chr, start = 1, stop = 5)
-    circ$chr<-str_remove(circ$chr,"_")
-    circ$chr<-str_remove(circ$chr,"chr")
-    circ$chr <- factor(circ$chr, levels = c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","Un","M","X","Y"))
-    
-    
-    # Add quality levels (score < 10 = Bad, score < 50 = Low, score < 200 = Medium, score > 200 = Good)
-    circ$quality <- cut(circ$score,breaks=c(-Inf,10,50,200,Inf),labels= c("Bad","Low", "Medium", "Good"),right = FALSE)
-    
-    # Add size of circle (number of bp)
-    circ$size_bp <- circ$end - circ$start
-    
-    circ
-    
-      ggplot(circ, aes(x = chr, y = size_bp))+
-        geom_point (aes(color=quality))+
-        theme_minimal()
+    # Import data and make it reactive
+    plot_circ <- reactive({
+      if (is.null(input$bedfile))
+        return(NULL) 
+      circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
+      
+      names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
+      
+      # Set chromosome as factor and fix random chr
+      circ$chr <- substr(circ$chr, start = 1, stop = 5)
+      circ$chr<-str_remove(circ$chr,"_")
+      circ$chr<-str_remove(circ$chr,"chr")
+      circ$chr <- factor(circ$chr, levels = c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","Un","M","X","Y"))
+      
+      
+      # Add quality levels (score < 10 = Bad, score < 50 = Low, score < 200 = Medium, score > 200 = Good)
+      circ$quality <- cut(circ$score,breaks=c(-Inf,10,50,200,Inf),labels= c("Bad","Low", "Medium", "Good"),right = FALSE)
+      
+      # Add circle id
+      circ <- circ %>% mutate(circle_id = 1:n()) %>% select(circle_id, everything())
+      
+      # Add size of circle (number of bp)
+      circ$size_bp <- circ$end - circ$start
+      
+      # Remove bad coverage circles and wrong discordant reads outputs
+      circ <- filter(circ,coverage_cont < 0.5 & discordant_reads < 5000)
+      
+      # Incorporate filters input (size and quality)
+      plot_circ<- filter(circ, between(size_bp, input$size[1], input$size[2]), quality == input$quality)
     })
+    
+   # Plot for circles under 5000bp
+    output$plot_results <- renderUI({
+      req(input$click)
+      
+      box(title="Circles under 5000bp",status="primary", width=8, renderGirafe({
+               gg_results <-ggplot(plot_circ(), aes( x = chr, y = size_bp,color=chr))+
+                geom_point_interactive (aes(tooltip=discordant_reads),size=4)+
+                 xlab("Original Chromosome")+
+                 ylab("Size")+
+                 theme_minimal()+
+                 guides(color="none")
+                
+              girafe(ggobj = gg_results)
+    })
+      )
+    })
+    
+    # Box of widgets for extra filtering the plot
+    output$filters<-renderUI({
+      req(input$click)
+      box(title="Filters (for plot only)", icon="filter",width=4,status="warning",
+        sliderInput(inputId = "size",
+                    "By size (number of base pairs):",
+                    min = 50,
+                    max= 5000,
+                    value = c(250,2500)),
+        checkboxGroupInput(inputId = "quality",
+                           "By quality:",
+                           choices = c("Bad","Low","Medium","Good"),
+                           selected= c("Medium","Good")),
+    )
+    })
+    
+  # TABLE OUTPUT
+    table_results<-reactive({
+      if (is.null(input$bedfile))
+        return(NULL)                
+      circ <-read.table(input$bedfile$datapath,header = FALSE, sep="\t",stringsAsFactors=FALSE)
+      names(circ) <- c("chr","start","end","discordant_reads","split_reads","score","coverage_mean","coverage_sd","coverage_start", "coverage_end","coverage_cont")
+      
+      # Add circle id
+      circ <- circ %>% mutate(circle_id = 1:n()) %>% select(circle_id, everything())
+      
+      # Add quality levels (score < 10 = Bad, score < 50 = Low, score < 200 = Medium, score > 200 = Good)
+      circ$quality <- cut(circ$score,breaks=c(-Inf,10,50,200,Inf),labels= c("Bad","Low", "Medium", "Good"),right = FALSE)
+      
+      # Add size of circle (number of bp)
+      circ$size_bp <- circ$end - circ$start
+      
+      # Remove bad coverage circles and outliers with wrong discordant reads outputs
+      circ <- filter(circ,coverage_cont < 0.5 & discordant_reads < 5000 & size_bp >5000)
+      })
+    
+    # Make the data table for circles over 5000bp
+    output$bigcircles <- DT::renderDataTable({
+      DT::datatable(table_results(),options = list(
+        searching = FALSE,
+        pageLength = 5,
+        lengthMenu = c(5, 10, 15, 20),
+        scrollX=TRUE
+      ))
+    })
+    
+    # Render UI - Appear after click on the first page
+    output$table_bigcircles <- renderUI({
+      req(input$click)
+      box(width = NULL, solidHeader = TRUE,
+          dataTableOutput("bigcircles"))
+    })
+    
 }
+##### "Circle info" tab server functions ####  
+
--- Visual EccDNA/ui.R
+++ Visual EccDNA/ui.R
@@ -1,10 +1,8 @@
-# Load required packages
+# Load required packages 
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
-library(ggplot2)
 library(ggiraph)
-library(plotly)
 library(DT)
 
 # User interface
@@ -13,9 +11,11 @@ ui <- dashboardPage(
     
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Get started",tabName="about",icon=icon("tasks")),
+            id="tabs",
+            menuItem("Get started",tabName="about",icon=icon("cloud-upload")),
             menuItem("View results", tabName ="results", icon=icon("bar-chart")),
-            menuItem("Circle Info",tabName="circle",icon=icon("circle-o"))
+            menuItem("Circle Info",tabName="circle",icon=icon("circle-o")),
+            menuItem("GitHub",tabName = "github",icon=icon("github"))
         )
     ),
     
@@ -26,7 +26,7 @@ ui <- dashboardPage(
                     fluidRow(
                         box(
                         title = "Wellcome to Visual eccDNA", status="primary",width = 12, solidHeader = TRUE,
-                        "This is an app for dynamic data visualization of eccDNA output files. Start selecting a BED file and this page will show an overview of the results. Then press Get Started! button, and you will be able to see further information about the DNA circles contained in your file, in View Results page."
+                        "This is an app for dynamic data visualization of eccDNA output files. Start selecting a BED file and this page will show an overview of the results. Then you can press the button View Results, and you will be able to see further information about the DNA circles contained in your file."
                     )),
                     fluidRow(
                             box(title= "Upload a Circle BED file with output:",fileInput("bedfile","Choose file:"),status="primary"),
@@ -39,16 +39,22 @@ ui <- dashboardPage(
             ),
             tabItem(
                 tabName = "results",
-                    fluidRow(
-                        box(plotOutput("results"),width=7,
-                            status = "primary",
-                            ),
+                    fluidRow(uiOutput("plot_results"),
+                             uiOutput("filters")),
+                    fluidRow(uiOutput("table_bigcircles")),
+                        
                     ),
-            ),
+
             tabItem(
                 tabName="circle",
-                        h2("Here goes the info of each circle")
-                )
+                        h2("Here goes the info of each circle"),
+                tags$img(src="under_construction.png",align="center")
+                ),
+            
+            tabItem(
+                tabName="github",
+                tags$a(href="https://github.com/agarcia18/eccDNA","https://github.com/agarcia18/eccDNA")
+            )
         )
     )
 )
